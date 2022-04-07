d70bdea40e8edded91bb6cedb582e124fe194c8a
cee breezy <djmisscbrown@hotmail.com>
2022-04-07 20:27:51 +0100

    amended auth file due to bug

--- utils/auth.js
+++ utils/auth.js
@@ -1,8 +1,10 @@
 // Verification of user login before access to route is given.
 const withAuth = (req, res, next) => {
   if (!req.session.userId) {
+    console.log("The user did not have a session")
     res.redirect("/login");
   } else {
+    console.log("The user did have a session")
     next();
   }
 };
