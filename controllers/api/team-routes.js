b6cb83031e0bb8fe6655dc5162302251ac048cfa
Jagdeep Tiwana <jtiwana@hotmail.com>
2022-04-09 11:10:39 +0100

    updated team routes

--- controllers/api/team-routes.js
+++ controllers/api/team-routes.js
@@ -2,6 +2,25 @@ const router = require("express").Router();
 const { User, Project, Team } = require("../../models");
 const withAuth = require("../../utils/auth");
 
+
+// identifying users within a team
+router.get('/:id', async (req, res) => {
+  try {
+    const teamData = await Team.findByPk(req.params.id, {
+      include: [{ model: User }]
+    });
+    if (!teamData) {
+      res.status(404).json({ message: 'No member(s) are found with that id!' });
+      return;
+    }
+    res.status(200).json(teamData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+
+
 router.post("/", withAuth, (req, res) => {
   const body = req.body;
 
