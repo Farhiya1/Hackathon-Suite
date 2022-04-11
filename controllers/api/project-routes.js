1728255112cdd8ea1d098c0ece2d39f8731ea63d
Jagdeep Tiwana <jtiwana@hotmail.com>
2022-04-11 20:50:34 +0100

    main

--- controllers/api/project-routes.js
+++ controllers/api/project-routes.js
@@ -2,12 +2,7 @@ const router = require("express").Router();
 const { Project, Team } = require("../../models");
 const withAuth = require("../../utils/auth");
 
-<<<<<<< HEAD
-
-router.get("/", withAuth, (req, res) => {
-=======
 router.get("/", (req, res) => {
->>>>>>> ef5cff25565b6f2185fdf204ccdbd9cb2fac5e06
   Project.findAll({
     include: [Team],
     where: {
