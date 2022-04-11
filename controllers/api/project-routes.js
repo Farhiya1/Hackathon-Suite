698f5d2869a718afb91df9e8f1a8de1c2c5ee128
Jagdeep Tiwana <jtiwana@hotmail.com>
2022-04-11 20:09:55 +0100

    added withAuth

--- controllers/api/project-routes.js
+++ controllers/api/project-routes.js
@@ -3,7 +3,7 @@ const { Project, Team } = require("../../models");
 const withAuth = require("../../utils/auth");
 
 
-router.get("/", (req, res) => {
+router.get("/", withAuth, (req, res) => {
   Project.findAll({
      include: [Team],
     where: {
