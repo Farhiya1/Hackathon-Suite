ef5cff25565b6f2185fdf204ccdbd9cb2fac5e06
Farhiya Mahamud <farhiya.i.mahamud@gmail.com>
2022-04-11 19:57:03 +0100

    fixed  res render "all-posts" from "/all-posts"

--- controllers/api/project-routes.js
+++ controllers/api/project-routes.js
@@ -2,23 +2,19 @@ const router = require("express").Router();
 const { Project, Team } = require("../../models");
 const withAuth = require("../../utils/auth");
 
-
 router.get("/", (req, res) => {
   Project.findAll({
-     include: [Team],
+    include: [Team],
     where: {
       team_id: null,
     },
-  }) 
-    .then((projectDBData) => {
-      const projects = projectDBData.map((project) =>
-        project.get({ plain: true })
-      );
-      console.log(projects);
-      res.render("/all-posts", { projects });
-    });
- 
+  }).then((projectDBData) => {
+    const projects = projectDBData.map((project) =>
+      project.get({ plain: true })
+    );
+    console.log(projects);
+    res.render("all-posts", { projects });
+  });
 });
-
 
 module.exports = router;
