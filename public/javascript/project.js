c041dddaddfa381768f198365e933116ab9042fc
Farhiya Mahamud <farhiya.i.mahamud@gmail.com>
2022-04-12 14:25:50 +0100

    added button to project js file for projects

--- public/javascript/project.js
+++ public/javascript/project.js
@@ -1,13 +1,23 @@
-function joinProject () {
-    fetch("/api/team", {
-        method: "post",
-        headers: { "Content-Type": "application/json" },
-      })
-        .then(function () {
-          ;
-        })
-        .catch((err) => console.log(err));
+// function joinProject () {
+//     fetch("/api/team", {
+//         method: "post",
+//         headers: { "Content-Type": "application/json" },
+//       })
+//         .then(function () {
+//           ;
+//         })
+//         .catch((err) => console.log(err));
+//     }
+
+//     document.querySelector("#projectBtn").addEventListener("click", joinProject);
+
+function loadDoc() {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById("project-button").innerHTML = this.responseText;
     }
-    
-    document.querySelector("#projectBtn").addEventListener("click", joinProject);
-    
+  };
+  xhttp.open("POST", "/api/team", true);
+  xhttp.send();
+}
