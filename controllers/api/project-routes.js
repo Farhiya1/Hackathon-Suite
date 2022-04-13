55391fbfb69065d000fbde698dda58838b88d957
Jagdeep Tiwana <jtiwana@hotmail.com>
2022-04-13 18:59:49 +0100

    updated code

--- controllers/api/project-routes.js
+++ controllers/api/project-routes.js
@@ -3,12 +3,11 @@ const { Project, Team } = require("../../models");
 const withAuth = require("../../utils/auth");
 
 router.get("/", (req, res) => {
+  console.log("/API/PROJECT ROUTE HIT")
   Project.findAll({
-    include: [Team],
-    where: {
-      team_id: null,
-    },
+    
   }).then((projectDBData) => {
+    console.log(projectDBData, "FROM FIND ALL PROJECT")
     const projects = projectDBData.map((project) =>
       project.get({ plain: true })
     );
