ea7902d12355519191bc974e023f30255d56b815
cee breezy <djmisscbrown@hotmail.com>
2022-04-14 11:31:08 +0100

    added hasone team to model

--- models/index.js
+++ models/index.js
@@ -19,4 +19,16 @@ Project.belongsToMany(User, {
   as: "members",
 });
 
+Team.belongsTo(Project, {
+  foreignKey: 'project_id'
+})
+
+Project.hasOne(Team, {
+  foreignKey: 'project_id'
+})
+
+// Project.hasOne(Team, {
+//   foreignKey: 'project_id'
+// })
+
 module.exports = { Project, User, Team };
