eb7d8c8a9a7193755c8382c60faacbc59101478a
Farhiya Mahamud <farhiya.i.mahamud@gmail.com>
2022-04-14 12:15:51 +0100

    added withAuth to projects page

--- controllers/api/project-routes.js
+++ controllers/api/project-routes.js
@@ -2,12 +2,12 @@ const router = require("express").Router();
 const { Project, Team } = require("../../models");
 const withAuth = require("../../utils/auth");
 
-router.get("/", (req, res) => {
-  console.log("/API/PROJECT ROUTE HIT")
+router.get("/", withAuth, (req, res) => {
+  console.log("/API/PROJECT ROUTE HIT");
   Project.findAll({
-    include: [Team ]
+    include: [Team],
   }).then((projectDBData) => {
-    console.log(projectDBData, "FROM FIND ALL PROJECT")
+    console.log(projectDBData, "FROM FIND ALL PROJECT");
     const projects = projectDBData.map((project) =>
       project.get({ plain: true })
     );
