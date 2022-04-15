abca23b0daa2db67f1a894bcdc5951137aea73e0
Farhiya Mahamud <farhiya.i.mahamud@gmail.com>
2022-04-15 09:58:00 +0100

    navbar toggler functionality js

--- public/javascript/homeprojects.js
+++ public/javascript/homeprojects.js
@@ -1,7 +1,8 @@
+// Navbar toggler button js functionionality applied
 document.addEventListener("click", function (event) {
-    if (event.target.classList.contains("navbar-toggler-icon")) {
-      document.getElementById("navbarResponsive").classList.toggle("show");
-    } else if (event.target.classList.contains("nav-link")) {
-      document.getElementById("navbarResponsive").classList.remove("show");
-    }
-  });
+  if (event.target.classList.contains("navbar-toggler-icon")) {
+    document.getElementById("navbarResponsive").classList.toggle("show");
+  } else if (event.target.classList.contains("nav-link")) {
+    document.getElementById("navbarResponsive").classList.remove("show");
+  }
+});
